491c544a376ebc7da294a98c4b525b0fed8675de
zoomdong <1344492820@qq.com>
2019-01-05 12:29:07 +0800

    add babel-plugin-import

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,20 @@
 import React, { Fragment } from 'react';
+import { Button } from 'antd';
 
 class Example extends React.Component{
+  state={
+    count:0
+  }
+  handleCrease = () =>{
+     this.setState((prevState)=>{
+        ++prevState.count;
+     })
+  }
   render(){
     return(
       <Fragment>
-       <div style={{fontSize:"32px",margin:'0 auto'}}> JUST TEST IT</div> 
+         <div style={{fontSize:"32px",textAlign:'center'}}> {`JUST TEST IT${this.state.count}`}</div> 
+         <Button onClick={this.handleCrease}>增加</Button>
       </Fragment>
     )
   }
